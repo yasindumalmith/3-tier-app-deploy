37aac3b5526712b24b696b41838ef26e60b0e395
yasindumalmith <yasindumalmith058@gmail.com>
2026-03-08 01:48:55 +0530

    create the startup sh file 1 2

--- web_startup.sh
+++ web_startup.sh
@@ -6,7 +6,7 @@ apt-get install -y nginx awscli
 systemctl start nginx
 systemctl enable nginx
 
-# S3 වලින් React build files ටික Nginx web folder එකට download කිරීම
+
 aws s3 cp s3://my-project-artifacts-bucket/frontend/ /var/www/html/ --recursive
 
 
