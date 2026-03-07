d5b278bf254f8102970b412f1bc9e3b3dc24112b
yasindumalmith <yasindumalmith058@gmail.com>
2026-03-08 02:48:18 +0530

    remove secret folder

--- terraform/modules/seceret/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "secret_arn" {
-  description = "The ARN of the database secret"
-  value       = aws_secretsmanager_secret.db_secret.arn
-}
-
-output "secret_name" {
-  description = "The name of the database secret"
-  value       = aws_secretsmanager_secret.db_secret.name
-}
